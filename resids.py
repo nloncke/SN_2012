ba42f074eddd2d4ed927268b53680959e626eb3f
Nicole Loncke <nloncke15@gmail.com>
2012-08-08 15:19:10 -0400

    Merged resids module with Shankar.

--- resids.py
+++ resids.py
@@ -18,48 +18,45 @@ def maxresidual(residuals, axis=None):
     """Finds the maximum residual from the input array.  NB: doesn't
     tell us whether it is negative or positive.
     """
-    # If you care about signed error, then uncomment this:
-    # indices = np.unravel_index(np.argmax(np.abs(residuals), axis),
-    #                            residuals.shape)
+
     return np.amax(np.abs(residuals), axis)
     
 
 def graphresiduals(day, N, resids, lams, dayarr, style='o-',
-                   save=True, ext='pdf'):
-    """Produces a graph of the residuals by the wavelengths for a
-    given day and a given n-degree approximation of the data.
+                   save=False, ext='pdf'):
+    """Graphs the residuals by the wavelengths for a given day and
+    an N-degree approximation for that day.
     """
 
     dayidx = specs.day_idx(day, dayarr)
-
     plt.semilogy(lams, np.abs(resids[N, dayidx, :]), style)
-    plt.title('{0}th Order Resids for Day{1}'.format(N, day))
-    plt.xlabel(r'log($\lambda$)')
-    plt.ylabel(r'log(Residuals)')
+
+    plt.xlabel(r'$\lambda$')
+    plt.ylabel(r'$log(Residuals)$')
+    plt.title('{0}th order Residuals for Day{1}'.format(N, day))
     plt.show()
 
     if save:
-        plt.savefig('day{0}_n{1}.{2}'.format(day, n, ext))
+        plt.savefig('day{0}-n{1}.{2}'.format(day, n, ext))
         print 'Figure saved!'
 
-    return
+    return None
 
 
 def graphmaxresiduals(resids, dayarr, day=None, display=True, save=False,
     style='o-', ext='pdf'):
-    """If day is specified, returns a plot of the maximum residuals
-    for a given day over all n's, where n is the number of principal
-    components used to recreate the data. If day is not specified,
-    produces a plot of the maximum residual for an nth order
-    approximation as a function of n.
+    """Returns a plot of the maximum residuals over all Ns for a
+    given day, or all days, if left unspecified, where N
+    is the number of principal components used to recreate the
+    data.
     """
     maxbyday = maxresidual(resids, axis=2)
 
     if day is not None:
-        day = specs.day_idx(day, dayarr)
-        yvals = maxbyday[:, day]
+        dayidx = specs.day_idx(day, dayarr)
+        yvals = maxbyday[:, dayidx]
         daystr = 'Day {0}'.format(day)
-        filename = 'day{0}resids'.format(day)
+        filename = 'day{0}alln'.format(day)
     else:
         daystr = 'All Days'
         yvals = np.amax(maxbyday, axis=1)
@@ -70,26 +67,10 @@ def graphmaxresiduals(resids, dayarr, day=None, display=True, save=False,
     plt.xlabel('N')
     plt.ylabel('Maximum residuals')
     
-    if display:
+    if display==True:
         plt.show()
 
-    if save:
+    if save==True:
         plt.savefig('{0}.{1}'.format(filename, ext))
 
     return np.array(yvals)
-
-
-
-# def allresids_by_n(resids, dayarr, ext='pdf'):
-#     """Maximum residual over all days vs. n-degree approximation."""
-    
-    
-#     # max_resids = maxresidual(np.array(max_resids), axis=0)
-
-#     # plt.plot(range(len(spex)), np.log(np.abs(max_resids)), 'o-')
-#     # plt.title('Maximum residual vs. Approximation Order N')
-#     # plt.xlabel('N')
-#     # plt.ylabel('log(maximum residual)')
-#     # plt.savefig('maxresids.{0}'.format(ext))
-    
-#     return np.array(max_resids)
